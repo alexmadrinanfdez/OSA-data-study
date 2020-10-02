148fc65d4c441ebc268165bd855c37f1c47b412a
Alejandro Madriñán <alejandro.madrinanfer@alumnos.upm.es>
2020-10-02 12:21:24 +0200

    ETL

---
+++ R/ETL.R
@@ -0,0 +1,68 @@
+# simple ETL process on a single Excel file
+
+# clear working space
+rm(list = ls())
+
+library(readxl)  # read Excel files
+library(dplyr)   # a grammar of data manipulation
+library(visdat)  # preliminary visualisation of data
+library(naniar)  # data structures, summaries, and visualisations for missing data
+library(tidyr)   # tidy messy data
+library(writexl) # export data frames to Excel '.xlsx' format
+library(stringr) # simple, consistent wrappers for common string operations
+
+i_file <- 'Info_BDApnea_QuironMalaga.xlsx'
+o_file <- 'DB.xlsx'
+directory <- '../data'
+
+## extract ##
+
+df_origin <- read_excel(paste(directory, i_file, sep = "/"))
+
+class(df_origin) # not ONLY a data.frame
+df_origin <- as.data.frame(df_origin)
+
+## transform (cleansing) ##
+
+glimpse(df_origin)
+
+# based on the BANG risk indicators:
+#   - BMI > 35
+#   - Age > 50 years
+#   - Neck circumference > 40 cm
+#   - Gender male
+
+# keep only meaningful predictors for diagnosis
+df_tmp <- 
+  df_origin %>% 
+    select(
+      patient = Patient,
+      gender = Gender,
+      IAH,
+      weight = Peso,
+      height = Talla,
+      age = Edad,
+      cervical = PerCervical,
+      smoker = Fumador # aditional predictor
+      )
+
+vis_dat(df_tmp) # at-a-glance ggplot object of what is inside a dataframe
+
+df_tmp <- df_tmp %>% replace_with_na_all(condition = ~.x == -1) # change all -1 values for NA
+df_tmp <- df_tmp %>% mutate(weight = as.numeric(weight))        # coerce char values to NA
+# df_tmp <- df_tmp %>% replace_with_na(replace = list(smoker = "ns"))
+df <- df_tmp %>% drop_na()                                      # drop rows containing missing values
+
+df <- 
+  df %>% 
+    mutate(
+      patient = str_trunc(patient, width = 3, side = "left", ellipsis = ""),
+      gender = as.factor(gender),      # gender as a two-level factor
+      BMI = weight / (height / 100)^2, # new column BMI = [kg]/[m]^2
+      smoker = as.factor(smoker)
+      )
+
+## load ##
+
+write_xlsx(df,
+           paste(directory, o_file, sep = "/" ))
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # OSA-diagnosis
 Basic study of the clinical data from various patients in order to ease the diagnosis of the Obstructive Sleep Apnea (OSA) disease.
+
+## ETL
+The first step is to study the data performing a an *ETL*:
+1. Extract the data from its sources
+2. Transform and tidy the data (cleansing)
+3. Load the data for storage
